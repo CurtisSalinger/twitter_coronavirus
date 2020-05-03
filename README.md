3ea0d0cfedc11dcbad639f8664782e5ae3bbb39c
Curtis Salinger <csalinger22@cmc.edu>
2020-05-03 11:11:40 -0700

    all finished

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# Coronavirus analysis
+# Coronavirus Twitter analysis
 
-This repo analyzes the activity of coronavirus hashtags on twitter.
+
+In this project, I analyzed every Tweet posted from September through the beginning of April looking for the spread of hashtags regarding Coronavirus.
+To do so, I wrote loops that looked only for tweets that had 2 specific attributes. The first is that they had a location asscociated with them, and
+the second is that they contained one of 17 hashtags that I was looking for including #coronavirus, #covid, #covid19, etc. I sorted this set of tweet
+two ways, once by language, and once by the country it was posted in. The goal of doing this is again so we could document how the information travel
+and popularity of Covid19 spread across the globe. As show in the files in the ``` viz``` folder, we can see that in terms of quantity, the US has 
+in general the most tweet's per Hashtag.
+
+
+This was a very interesting project and provided a great introduction into using python and shell coding to interpret big data.
--- src/map.py
+++ src/map.py
@@ -16,63 +16,73 @@ from collections import Counter,defaultdict
 
 # load keywords
 hashtags = [
-    '#코로나바이러스',  # korean
-    '#コロナウイルス',  # japanese
-    '#冠状病毒',        # chinese
-    '#covid2019',
-    '#covid-2019',
-    '#covid19',
-    '#covid-19',
-    '#coronavirus',
-    '#corona',
-    '#virus',
-    '#flu',
-    '#sick',
-    '#cough',
-    '#sneeze',
-    '#hospital',
-    '#nurse',
-    '#doctor',
-    ]
+	'#코로나바이러스',  # korean
+	'#コロナウイルス',  # japanese
+	'#冠状病毒',        # chinese
+	'#covid2019',
+	'#covid-2019',
+	'#covid19',
+	'#covid-19',
+	'#coronavirus',
+	'#corona',
+	'#virus',
+	'#flu',
+	'#sick',
+	'#cough',
+	'#sneeze',
+	'#hospital',
+	'#nurse',
+	'#doctor',
+	]
 
 # initialize counters
 counter_lang = defaultdict(lambda: Counter())
+counter_country = defaultdict(lambda: Counter())
 
 # open the zipfile
 with zipfile.ZipFile(args.input_path) as archive:
 
-    # loop over every file within the zip file
-    for i,filename in enumerate(archive.namelist()):
-        print(datetime.datetime.now(),args.input_path,filename)
+	# loop over every file within the zip file
+	for i,filename in enumerate(archive.namelist()):
+		print(datetime.datetime.now(),args.input_path,filename)
 
-        # open the inner file
-        with archive.open(filename) as f:
+		# open the inner file
+		with archive.open(filename) as f:
 
-            # loop over each line in the inner file
-            for line in f:
+			# loop over each line in the inner file
+			for line in f:
+				# load the tweet as a python dictionary
+				tweet = json.loads(line)
 
-                # load the tweet as a python dictionary
-                tweet = json.loads(line)
+				# convert text to lower case
+				text = tweet['text'].lower()
+				try:	#make country_code 'und' if None
+					loc = tweet['place']['country_code'].lower()
 
-                # convert text to lower case
-                text = tweet['text'].lower()
-
-                # search hashtags
-                for hashtag in hashtags:
-                    lang = tweet['lang']
-                    if hashtag in text:
-                        counter_lang[hashtag][lang] += 1
-                    counter_lang['_all'][lang] += 1
-
+				except TypeError:
+					loc = 'und'
+					pass
+				# search hashtags
+				for hashtag in hashtags:
+					lang = tweet['lang']
+					if hashtag in text:
+						counter_lang[hashtag][lang] += 1
+						counter_country[hashtag][loc] += 1
+					counter_lang['_all'][lang] += 1
+					counter_country['_all'][loc] += 1
 # open the outputfile
 try:
-    os.makedirs(args.output_folder)
+	os.makedirs(args.output_folder)
 except FileExistsError:
-    pass
+	pass
 output_path_base = os.path.join(args.output_folder,os.path.basename(args.input_path))
 
 output_path_lang = output_path_base+'.lang'
 print('saving',output_path_lang)
 with open(output_path_lang,'w') as f:
-    f.write(json.dumps(counter_lang))
+	f.write(json.dumps(counter_lang))
 
+output_path_country = output_path_base+'.country'
+print('saving',output_path_country)
+with open(output_path_country,'w') as f:
+	f.write(json.dumps(counter_country))
